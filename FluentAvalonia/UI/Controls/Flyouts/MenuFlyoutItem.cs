a621addbed9e3dded93fbc54b75c0981bf0e386c
amwx <armccormickwx@gmail.com>
2021-07-15 22:45:35 -0500

    New MenuFlyouts

---
+++ FluentAvalonia/UI/Controls/Flyouts/MenuFlyoutItem.cs
@@ -0,0 +1,260 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.LogicalTree;
+using FluentAvalonia.UI.Input;
+using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace FluentAvalonia.UI.Controls
+{
+	public class MenuFlyoutItem : MenuFlyoutItemBase, IMenuItem, ICommandSource
+	{
+		public static readonly StyledProperty<string> TextProperty =
+			AvaloniaProperty.Register<MenuFlyoutItem, string>(nameof(Text));
+
+		public static readonly StyledProperty<IconElement> IconProperty =
+			AvaloniaProperty.Register<MenuFlyoutItem, IconElement>(nameof(Icon));
+
+		public static readonly DirectProperty<MenuFlyoutItem, ICommand> CommandProperty =
+			Button.CommandProperty.AddOwner<MenuFlyoutItem>(x => x.Command,
+				(x, v) => x.Command = v);
+
+		public static readonly StyledProperty<object> CommandParameterProperty =
+			Button.CommandParameterProperty.AddOwner<MenuFlyoutItem>();
+
+		public static readonly StyledProperty<KeyGesture> HotKeyProperty =
+			Button.HotKeyProperty.AddOwner<MenuFlyoutItem>();
+
+
+		public string Text
+		{
+			get => GetValue(TextProperty);
+			set => SetValue(TextProperty, value);
+		}
+
+		public IconElement Icon
+		{
+			get => GetValue(IconProperty);
+			set => SetValue(IconProperty, value);
+		}
+
+		public KeyGesture HotKey
+		{
+			get => GetValue(HotKeyProperty);
+			set => SetValue(HotKeyProperty, value);
+		}
+
+		public ICommand Command
+		{
+			get => _command;
+			set => SetAndRaise(CommandProperty, ref _command, value);
+		}
+
+		public object CommandParameter
+		{
+			get => GetValue(CommandParameterProperty);
+			set => SetValue(CommandParameterProperty, value);
+		}
+
+		protected override bool IsEnabledCore => base.IsEnabledCore && _canExecute;
+
+		public static readonly RoutedEvent<RoutedEventArgs> ClickEvent =
+			RoutedEvent.Register<MenuFlyoutItem, RoutedEventArgs>(nameof(Click),
+			  RoutingStrategies.Bubble);
+
+		public event EventHandler<RoutedEventArgs> Click
+		{
+			add => AddHandler(ClickEvent, value);
+			remove => RemoveHandler(ClickEvent, value);
+		}
+
+
+		bool IMenuItem.HasSubMenu => false;
+		bool IMenuItem.IsPointerOverSubMenu => false;
+		bool IMenuItem.IsSubMenuOpen { get => false; set { } }
+		public bool IsTopLevel => false;
+		IMenuItem IMenuElement.SelectedItem { get => null; set { } }
+		IEnumerable<IMenuItem> IMenuElement.SubItems => null;
+		IMenuElement IMenuItem.Parent
+		{
+			get
+			{
+				if (this.FindLogicalAncestorOfType<MenuFlyoutSubItem>() is MenuFlyoutSubItem mfsi)
+				{
+					return mfsi;
+				}
+
+				return Parent as IMenuElement;
+			}
+		}
+
+
+		protected override void OnAttachedToLogicalTree(LogicalTreeAttachmentEventArgs e)
+		{
+			if (_hotkey != null)
+			{
+				HotKey = _hotkey;
+			}
+
+			base.OnAttachedToLogicalTree(e);
+
+			if (Command != null)
+			{
+				Command.CanExecuteChanged += CanExecuteChanged;
+				CanExecuteChanged(this, null);
+			}
+		}
+
+		protected override void OnDetachedFromLogicalTree(LogicalTreeAttachmentEventArgs e)
+		{
+			if (HotKey != null)
+			{
+				_hotkey = HotKey;
+				HotKey = null;
+			}
+
+			base.OnDetachedFromLogicalTree(e);
+
+			if (Command != null)
+			{
+				Command.CanExecuteChanged -= CanExecuteChanged;
+			}
+		}
+
+		protected override void OnPropertyChanged<T>(AvaloniaPropertyChangedEventArgs<T> change)
+		{
+			base.OnPropertyChanged(change);
+
+			if (change.Property == CommandProperty)
+			{
+				var oldCommand = change.OldValue.GetValueOrDefault() as ICommand;
+				var newCommand = change.NewValue.GetValueOrDefault() as ICommand;
+
+				if (oldCommand is XamlUICommand oldXaml)
+				{
+					if (Text == oldXaml.Label)
+					{
+						Text = null;
+					}
+
+					if (Icon is IconSourceElement ele && ele.IconSource == oldXaml.IconSource)
+					{
+						Icon = null;
+					}
+
+					if (HotKey == oldXaml.HotKey)
+					{
+						HotKey = null;
+					}
+				}
+
+				if (newCommand is XamlUICommand newXaml)
+				{
+					if (string.IsNullOrEmpty(Text))
+					{
+						Text = newXaml.Label;
+					}
+
+					if (Icon == null)
+					{
+						Icon = new IconSourceElement { IconSource = newXaml.IconSource };
+					}
+
+					if (HotKey == null)
+					{
+						HotKey = newXaml.HotKey;
+					}
+				}
+
+				if (((ILogical)this).IsAttachedToLogicalTree)
+				{
+					if (oldCommand != null)
+					{
+						oldCommand.CanExecuteChanged -= CanExecuteChanged;
+					}
+
+					if (newCommand != null)
+					{
+						newCommand.CanExecuteChanged += CanExecuteChanged;
+					}
+				}
+
+				CanExecuteChanged(this, null);
+			}
+			else if (change.Property == CommandParameterProperty)
+			{
+				CanExecuteChanged(this, null);
+			}
+			else if (change.Property == HotKeyProperty)
+			{
+				PseudoClasses.Set(":hotkey", change.NewValue.GetValueOrDefault() != null);
+			}
+		}
+
+		protected override void OnPointerPressed(PointerPressedEventArgs e)
+		{
+			base.OnPointerPressed(e);
+			if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+			{
+				PseudoClasses.Set(":pressed", true);
+			}
+		}
+
+		protected override void OnPointerReleased(PointerReleasedEventArgs e)
+		{
+			base.OnPointerReleased(e);
+			if (e.InitialPressMouseButton == MouseButton.Left)
+			{
+				PseudoClasses.Set(":pressed", false);
+			}
+		}
+
+		void IMenuElement.Close() { }
+		void IMenuElement.Open() { }
+
+		public bool MoveSelection(NavigationDirection direction, bool wrap)
+		{
+			return false;
+		}
+
+		public void RaiseClick()
+		{
+			OnClick();
+		}
+
+		protected virtual void OnClick()
+		{
+			RaiseEvent(new RoutedEventArgs(MenuItem.ClickEvent));
+
+			if (Command?.CanExecute(CommandParameter) == true)
+			{
+				Command.Execute(CommandParameter);
+			}
+		}
+
+		private void CanExecuteChanged(object sender, EventArgs e)
+		{
+			var canExec = Command == null || Command.CanExecute(CommandParameter);
+
+			if (canExec != _canExecute)
+			{
+				_canExecute = canExec;
+				UpdateIsEffectivelyEnabled();
+			}
+		}
+
+		protected override void OnGotFocus(GotFocusEventArgs e)
+		{
+			base.OnGotFocus(e);
+		}
+
+		void ICommandSource.CanExecuteChanged(object sender, EventArgs e) => this.CanExecuteChanged(sender, e);
+
+		private ICommand _command;
+		private bool _canExecute = true;
+		private KeyGesture _hotkey;
+	}
+}
---
+++ FluentAvalonia/UI/Controls/Flyouts/MenuFlyoutSubItem.cs
@@ -0,0 +1,205 @@
+﻿using Avalonia.Collections;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Templates;
+using Avalonia.Input;
+using Avalonia.Metadata;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FluentAvalonia.UI.Controls
+{
+	public class MenuFlyoutSubItem : MenuFlyoutItemBase, IMenuItem
+	{
+		public MenuFlyoutSubItem()
+		{
+			_items = new AvaloniaList<object>();
+		}
+
+		public static readonly StyledProperty<string> TextProperty =
+			AvaloniaProperty.Register<MenuFlyoutItem, string>(nameof(Text));
+
+		public static readonly StyledProperty<IconElement> IconProperty =
+			AvaloniaProperty.Register<MenuFlyoutItem, IconElement>(nameof(Icon));
+
+		public static readonly DirectProperty<MenuFlyoutSubItem, IEnumerable> ItemsProperty =
+			ItemsControl.ItemsProperty.AddOwner<MenuFlyoutSubItem>(x => x.Items,
+				(x, v) => x.Items = v);
+
+		public static readonly StyledProperty<IDataTemplate> ItemTemplateProperty =
+			ItemsControl.ItemTemplateProperty.AddOwner<MenuFlyoutSubItem>();
+
+		public string Text
+		{
+			get => GetValue(TextProperty);
+			set => SetValue(TextProperty, value);
+		}
+
+		public IconElement Icon
+		{
+			get => GetValue(IconProperty);
+			set => SetValue(IconProperty, value);
+		}
+
+		[Content]
+		public IEnumerable Items
+		{
+			get => _items;
+			set => SetAndRaise(ItemsProperty, ref _items, value);
+		}
+
+		public bool HasSubMenu => true;
+		public bool IsPointerOverSubMenu => _subMenu?.IsPointerOverPopup ?? false;
+		public bool IsSubMenuOpen
+		{
+			get => _subMenu?.IsOpen ?? false;
+			set
+			{
+				if (value)
+					Open();
+				else
+					Close();
+			}
+		}
+		public bool IsTopLevel => false;
+		IMenuElement IMenuItem.Parent => Parent as IMenuElement;
+		public IMenuItem SelectedItem
+		{
+			get
+			{
+				if (_presenter != null && _presenter.SelectedIndex != -1)
+				{
+					return _presenter.ItemContainerGenerator.ContainerFromIndex(_presenter.SelectedIndex) as IMenuItem;
+				}
+
+				return null;
+			}
+			set
+			{
+				if (_presenter != null)
+				{
+					_presenter.SelectedIndex = _presenter.ItemContainerGenerator.IndexFromContainer(value);
+				}
+			}
+		}
+		IEnumerable<IMenuItem> IMenuElement.SubItems
+		{
+			get
+			{
+				return _presenter?.ItemContainerGenerator.Containers
+					.Select(x => x.ContainerControl)
+					.OfType<IMenuItem>();
+			}
+		}
+
+
+		void IMenuItem.RaiseClick()
+		{ }
+
+		protected override void OnPointerEnter(PointerEventArgs e)
+		{
+			base.OnPointerEnter(e);
+
+			var point = e.GetCurrentPoint(null);
+			RaiseEvent(new PointerEventArgs(MenuItem.PointerEnterItemEvent, this, e.Pointer, this.VisualRoot, point.Position,
+				e.Timestamp, point.Properties, e.KeyModifiers));
+		}
+
+		protected override void OnPointerLeave(PointerEventArgs e)
+		{
+			base.OnPointerLeave(e);
+
+			var point = e.GetCurrentPoint(null);
+			RaiseEvent(new PointerEventArgs(MenuItem.PointerLeaveItemEvent, this, e.Pointer, this.VisualRoot, point.Position,
+				e.Timestamp, point.Properties, e.KeyModifiers));
+		}
+
+		public void Open()
+		{
+			InitPopup();
+			_subMenu.IsOpen = true;
+		}
+
+		public void Close()
+		{
+			_subMenu.IsOpen = false;
+			_presenter.SelectedIndex = -1;
+		}
+
+		public bool MoveSelection(NavigationDirection direction, bool wrap)
+		{
+			if (_presenter.SelectedIndex == -1)
+			{
+				int index = 0;
+				var ct = _presenter.ItemCount;
+				for (int i = 0; i < ct; i++)
+				{
+					if (_presenter.ItemContainerGenerator.ContainerFromIndex(i).Focusable)
+					{
+						index = i;
+						break;
+					}
+				}
+
+				FocusManager.Instance.Focus(_presenter.ItemContainerGenerator.ContainerFromIndex(index), NavigationMethod.Directional);
+				_presenter.SelectedIndex = index;
+
+				return true;
+			}
+
+			var sel = _presenter?.InternalMoveSelection(direction, wrap) ?? false;
+			if (sel)
+			{
+				FocusManager.Instance.Focus(_presenter.ItemContainerGenerator.ContainerFromIndex(_presenter.SelectedIndex), NavigationMethod.Directional);
+			}
+
+			return sel;
+		}
+
+		private void InitPopup()
+		{
+			if (_subMenu == null)
+			{
+				_presenter = new MenuFlyoutPresenter()
+				{
+					[!ItemsControl.ItemsProperty] = this[!ItemsProperty],
+					[!ItemsControl.ItemTemplateProperty] = this[!ItemTemplateProperty]
+				};
+
+				_subMenu = new Popup
+				{
+					Child = _presenter,
+					HorizontalOffset = -4,
+					WindowManagerAddShadowHint = false,
+					PlacementMode = PlacementMode.AnchorAndGravity,
+					PlacementAnchor = Avalonia.Controls.Primitives.PopupPositioning.PopupAnchor.TopRight,
+					PlacementGravity = Avalonia.Controls.Primitives.PopupPositioning.PopupGravity.BottomRight,
+					PlacementTarget = this
+				};
+
+				LogicalChildren.Add(_subMenu);
+
+				_subMenu.Opened += OnPopupOpen;
+				_subMenu.Closed += OnPopupClose;
+			}
+		}
+
+		private void OnPopupOpen(object sender, EventArgs e)
+		{
+			PseudoClasses.Set(":submenuopen", true);
+		}
+
+		private void OnPopupClose(object sender, EventArgs e)
+		{
+			PseudoClasses.Set(":submenuopen", false);
+		}
+
+
+		private Popup _subMenu;
+		private MenuFlyoutPresenter _presenter;
+		private IEnumerable _items;
+	}
+}
