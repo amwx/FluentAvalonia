765f2db48294ab956fbbe899a15d7330f68b9a42
amwx <armccormickwx@gmail.com>
2021-07-20 14:56:04 -0500

    NavigationView & DataTemplateSelector

---
+++ FluentAvalonia/UI/Controls/DataTemplateSelector.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+
+namespace FluentAvalonia.UI.Controls
+{
+	public class DataTemplateSelector
+	{
+		public IDataTemplate SelectTemplate(object item) => SelectTemplateCore(item);
+
+		public IDataTemplate SelectTemplate(object item, IControl container) => SelectTemplateCore(item, container);
+
+		protected virtual IDataTemplate SelectTemplateCore(object item) => null;
+
+		protected virtual IDataTemplate SelectTemplateCore(object item, IControl container) => null;
+	}
+}
--- FluentAvalonia/UI/Controls/NavigationView/NavigationViewItemsFactory.cs
+++ FluentAvalonia/UI/Controls/NavigationView/NavigationViewItemsFactory.cs
@@ -1,8 +1,9 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
 using Avalonia.Controls.Templates;
-using System;
+using Avalonia.Logging;
 using System.Collections.Generic;
-using System.Text;
 
 namespace FluentAvalonia.UI.Controls
 {
@@ -21,10 +22,10 @@ namespace FluentAvalonia.UI.Controls
                 {
                     _itemTemplateWrapper = new ItemTemplateWrapper(dt);
                 }
-                //else if (newValue is DataTemplates dts)
-                //{
-                //    _itemTemplateWrapper = new ItemTemplateWrapper(dts);
-                //}
+                else if (newValue is DataTemplateSelector dts)
+                {
+                    _itemTemplateWrapper = new ItemTemplateWrapper(dts);
+                }
             }
 
             _navViewPool = new List<NavigationViewItem>(4);
@@ -143,64 +144,93 @@ namespace FluentAvalonia.UI.Controls
         private List<NavigationViewItem> _navViewPool;
     }
 
-    //Taken from Avalonia
+    
     internal class ItemTemplateWrapper : IElementFactory
     {
+		// Internal property to RecyclePool, we'll expose here
+		public static readonly AttachedProperty<IDataTemplate> OriginTemplateProperty =
+			AvaloniaProperty.RegisterAttached<ItemTemplateWrapper, IControl, IDataTemplate>("OriginTemplate");
+
         private readonly IDataTemplate _dataTemplate;
+		private readonly DataTemplateSelector _dataTemplateSelector;
 
         public ItemTemplateWrapper(IDataTemplate dataTemplate) => _dataTemplate = dataTemplate;
 
-        public IControl Build(object param) => GetElement(null, param);
-        public bool Match(object data) => _dataTemplate.Match(data);
+		public ItemTemplateWrapper(DataTemplateSelector dts) => _dataTemplateSelector = dts;
+
+		// These can be safely ignored since this is internal & we only call
+		// GetElement and RecycleElement
+        public IControl Build(object param) => null;
+		public bool Match(object data) => false;
 
         public IControl GetElement(ElementFactoryGetArgs args)
         {
-            return GetElement(args.Parent, args.Data);
-        }
+			var selectedTemplate = _dataTemplate ?? _dataTemplateSelector.SelectTemplate(args.Data);
 
-        public void RecycleElement(ElementFactoryRecycleArgs args)
+			// Check if selected template we got is valid
+			if (selectedTemplate == null)
+			{
+				// Null template, use other SelectTemplate method
+				selectedTemplate = _dataTemplateSelector.SelectTemplate(args.Data, null);
+
+				// WinUI errors out here, we'll just use FuncDataTemplate.Default
+				if (selectedTemplate == null)
+				{
+					selectedTemplate = FuncDataTemplate.Default;
+					Logger.TryGet(LogEventLevel.Information, "NavigationViewItemsFactory")?
+						.Log("", $"No DataTemplate found for type {args.Data.GetType()}. Using default instead");
+				}
+			}
+
+			var recPool = RecyclePool.GetPoolInstance(selectedTemplate);
+			IControl element = null;
+
+			if (recPool != null)
+			{
+				element = recPool.TryGetElement(string.Empty, args.Parent);
+			}
+			
+			if (element == null)
+			{
+				// no element was found in recycle pool, create a new element
+				element = selectedTemplate.Build(args.Data);
+
+				// Template returned null, so insert empty element to render nothing
+				// We shouldn't encounter this here, but just in case
+				if (element == null)
+				{
+					element = new Rectangle();
+				}
+
+				element.SetValue(OriginTemplateProperty, selectedTemplate);
+			}
+
+			// I believe DataTemplate.LoadContent() in WinUI also applies the DataContext, so we'll do
+			// that here. If we don't, for some reason, we can get additional elements in the ItemsRepeater
+			// For example, comment out the line below & run the sample app, scroll to the DataBinding
+			// NavView example, open dev tools and scope to one of the NVIs to find the parent ItemsRepeater
+			// You'll see there are 4 NVIs, which is correct, but you'll see more than one NVISeparator
+			// only one is correctly arranged, any additional are arranged offscreen
+			// I cannot figure out why this is happening, and I have no idea whether its a bug in WinUI,
+			// the Avalonia port, or something I'm doing. This seems to fix it though, so YEET
+			element.DataContext = args.Data;
+			return element;
+		}
+
+		public void RecycleElement(ElementFactoryRecycleArgs args)
         {
-            RecycleElement(args.Parent, args.Element);
-        }
+			var element = args.Element;
+			IDataTemplate selectedTemplate = _dataTemplate ?? element.GetValue(OriginTemplateProperty);
 
-        private IControl GetElement(IControl parent, object data)
-        {
-            var selectedTemplate = _dataTemplate;
-            var recyclePool = RecyclePool.GetPoolInstance(selectedTemplate);
-            IControl element = null;
+			var recPool = RecyclePool.GetPoolInstance(selectedTemplate);
 
-            if (recyclePool != null)
-            {
-                // try to get an element from the recycle pool.
-                element = recyclePool.TryGetElement(string.Empty, parent);
-            }
+			if (recPool == null)
+			{
+				recPool = new RecyclePool();
+				RecyclePool.SetPoolInstance(selectedTemplate, recPool);
+			}
 
-            if (element == null)
-            {
-                // no element was found in recycle pool, create a new element
-                element = selectedTemplate.Build(data);
-
-                // Associate template with element
-                //**NOTE: Can't do this, since property is internal to Avalonia
-                //        Not sure what it does, but it doesn't seem to have any adverse effects
-                //element.SetValue(RecyclePool.OriginTemplateProperty, selectedTemplate);
-            }
-
-            return element;
-        }
-
-        private void RecycleElement(IControl parent, IControl element)
-        {
-            var selectedTemplate = _dataTemplate;
-            var recyclePool = RecyclePool.GetPoolInstance(selectedTemplate);
-            if (recyclePool == null)
-            {
-                // No Recycle pool in the template, create one.
-                recyclePool = new RecyclePool();
-                RecyclePool.SetPoolInstance(selectedTemplate, recyclePool);
-            }
-
-            recyclePool.PutElement(element, "" /* key */, parent);
+			recPool.PutElement(args.Element, string.Empty, args.Parent);
         }
     }
 }
