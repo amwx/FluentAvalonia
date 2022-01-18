7f8697ae263b0776bf13b911be8813b69e8cb4bf
amwx <40413319+amwx@users.noreply.github.com>
2022-01-18 18:34:31 -0500

    Frame

--- FluentAvalonia/UI/Controls/Frame/NavigationEventArgs.cs
+++ FluentAvalonia/UI/Controls/Frame/NavigationEventArgs.cs
@@ -3,9 +3,23 @@ using System;
 
 namespace FluentAvalonia.UI.Navigation
 {
+    /// <summary>
+    /// Represents the method that will handle the Navigated event.
+    /// </summary>
+    /// <param name="sender">The object where the handler is attached.</param>
+    /// <param name="e">Event data for the event.</param>
     public delegate void NavigatedEventHandler(object sender, NavigationEventArgs e);
+
+    /// <summary>
+    /// Provides event data for the NavigationStopped event.
+    /// </summary>
+    /// <param name="sender">The object where the handler is attached.</param>
+    /// <param name="e">Event data for the event.</param>
     public delegate void NavigationStoppedEventHandler(object sender, NavigationEventArgs e);
 
+    /// <summary>
+    /// Provides data for navigation methods and event handlers that cannot cancel the navigation request.
+    /// </summary>
     public class NavigationEventArgs : EventArgs
     {
         internal NavigationEventArgs(object content, NavigationMode mode,
@@ -20,10 +34,30 @@ namespace FluentAvalonia.UI.Navigation
         }
 
         //public Uri Uri { get; set; }
+
+        /// <summary>
+        /// Gets the root node of the target page's content.
+        /// </summary>
         public object Content { get; }
+
+        /// <summary>
+        /// Gets a value that indicates the direction of movement during navigation
+        /// </summary>
         public NavigationMode NavigationMode { get; }
+
+        /// <summary>
+        /// Gets any "Parameter" object passed to the target page for the navigation.
+        /// </summary>
         public object Parameter { get; }
+
+        /// <summary>
+        /// Gets the data type of the source page.
+        /// </summary>
         public Type SourcePageType { get; }
+
+        /// <summary>
+        /// Gets a value that indicates the animated transition associated with the navigation.
+        /// </summary>
         public NavigationTransitionInfo NavigationTransitionInfo { get; }
     }
 }
