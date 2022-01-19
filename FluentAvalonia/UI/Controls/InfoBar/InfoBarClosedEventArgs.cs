52f2753f4691c6f807eacbe95d7bafb547f6cd20
amwx <40413319+amwx@users.noreply.github.com>
2022-01-18 21:45:04 -0500

    InfoBar and InfoBadge

---
+++ FluentAvalonia/UI/Controls/InfoBar/InfoBarClosedEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FluentAvalonia.UI.Controls
+{
+	/// <summary>
+	/// Provides data for the InfoBar.Closed event.
+	/// </summary>
+	public class InfoBarClosedEventArgs : EventArgs
+	{
+		internal InfoBarClosedEventArgs(InfoBarCloseReason reason)
+		{
+			Reason = reason;
+		}
+
+		/// <summary>
+		/// Gets a constant that specifies whether the cause of the Closed event 
+		/// was due to user interaction (Close button click) or programmatic closure.
+		/// </summary>
+		public InfoBarCloseReason Reason { get; }
+	}
+}
--- FluentAvalonia/UI/Controls/InfoBar/InfoBarEnums.cs
+++ FluentAvalonia/UI/Controls/InfoBar/InfoBarEnums.cs
@@ -1,16 +1,52 @@
 ﻿namespace FluentAvalonia.UI.Controls
 {
+	/// <summary>
+	/// Defines constants that indicate the cause of the InfoBar closure.
+	/// </summary>
 	public enum InfoBarCloseReason
 	{
+		/// <summary>
+		/// The InfoBar was closed by the user clicking the close button.
+		/// </summary>
 		CloseButton = 0,
+
+		/// <summary>
+		/// The InfoBar was programmatically closed.
+		/// </summary>
 		Programmatic = 1
 	}
 
+	/// <summary>
+	/// Defines constants that indicate the criticality of the InfoBar that is shown.
+	/// </summary>
 	public enum InfoBarSeverity
 	{
+		/// <summary>
+		/// Communicates that the InfoBar is displaying general information that requires 
+		/// the user's attention. For assistive technologies, they will follow the 
+		/// behavior set in the Processing_All constant.
+		/// </summary>
 		Informational = 0,
+
+		/// <summary>
+		/// Communicates that the InfoBar is displaying information regarding a long-running 
+		/// and/or background task that has completed successfully. For assistive technologies,
+		/// they will follow the behavior set in the Processing_All constant.
+		/// </summary>
 		Success = 1,
+
+		/// <summary>
+		/// Communicates that the InfoBar is displaying information regarding a condition that 
+		/// might cause a problem in the future. For assistive technologies, they will follow 
+		/// the behavior set in the NotificationProcessing_ImportantAll constan
+		/// </summary>
 		Warning = 2,
+
+		/// <summary>
+		/// Communicates that the InfoBar is displaying information regarding an error or problem 
+		/// that has occurred. For assistive technologies, they will follow the behavior set in the 
+		/// NotificationProcessing_ImportantAll constant.
+		/// </summary>
 		Error = 3,
 	}
 }
