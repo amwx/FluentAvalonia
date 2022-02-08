90a0250b1b657218e1419f67ea74660599b3af0e
amwx <40413319+amwx@users.noreply.github.com>
2022-02-07 23:01:45 -0500

    Port TabView styles

---
+++ FluentAvalonia/Converters/ScrollViewerVisibilityToBoolConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Controls.Primitives;
+using Avalonia.Data.Converters;
+
+namespace FluentAvalonia.Converters
+{
+    public class ScrollViewerVisibilityToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((ScrollBarVisibility)value) == ScrollBarVisibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
