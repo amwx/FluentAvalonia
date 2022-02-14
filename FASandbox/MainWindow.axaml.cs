bd69aba2b3f0f3e16a49a8f01691f0f33d7fecc2
amwx <40413319+amwx@users.noreply.github.com>
2022-02-14 17:27:16 -0500

    Reset sandbox project

--- FASandbox/MainWindow.axaml.cs
+++ FASandbox/MainWindow.axaml.cs
@@ -1,12 +1,11 @@
+using System;
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Input;
 using Avalonia.Markup.Xaml;
-using FluentAvalonia.UI.Controls;
 
 namespace FASandbox
 {
-	public partial class MainWindow : Window
+    public partial class MainWindow : Window
 	{
 		public MainWindow()
 		{
@@ -14,16 +13,9 @@ namespace FASandbox
 #if DEBUG
             this.AttachDevTools();
 #endif
-
-            var tv2 = this.FindControl<TabView>("TabView2");
-            tv2.TabStripDragOver += Tv2_TabStripDragOver;
+            DataContext = new MainWindowViewModel();            
 		}
-
-        private void Tv2_TabStripDragOver(object sender, DragEventArgs e)
-        {
-            e.DragEffects = DragDropEffects.Move | DragDropEffects.Copy | DragDropEffects.Link;
-        }
-
+       
         private void InitializeComponent()
 		{
 			AvaloniaXamlLoader.Load(this);
