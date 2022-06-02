f42ff9e440ff135f01bd01cac9c6707ef38899c5
amwx <40413319+amwx@users.noreply.github.com>
2022-06-01 23:07:02 -0400

    Add IApplicationSplashScreen

---
+++ FluentAvalonia/Core/ApplicationModel/IApplicationSplashScreen.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Avalonia.Media;
+
+namespace FluentAvalonia.Core.ApplicationModel
+{
+    public interface IApplicationSplashScreen
+    {
+        string AppName { get; }
+
+        IImage AppIcon { get; }
+
+        object SplashScreenContent { get; }
+
+        void RunTasks();
+
+        int MinimumShowTime { get; }
+    }
+}
