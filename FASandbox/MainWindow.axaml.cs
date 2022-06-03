4dcf745bfbaf3b40f6a74b49f3b20cd38ba505d2
amwx <40413319+amwx@users.noreply.github.com>
2022-06-03 16:48:45 -0400

    Move to net6.0 target

--- FASandbox/MainWindow.axaml.cs
+++ FASandbox/MainWindow.axaml.cs
@@ -3,22 +3,21 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
-namespace FASandbox
-{
-    public partial class MainWindow : Window
+namespace FASandbox;
+
+public partial class MainWindow : Window
 	{
-		public MainWindow()
-		{
-			InitializeComponent();
+	public MainWindow()
+	{
+		InitializeComponent();
 #if DEBUG
-            this.AttachDevTools();
+        this.AttachDevTools();
 #endif
-            DataContext = new MainWindowViewModel();            
-		}
-       
-        private void InitializeComponent()
+    DataContext = new MainWindowViewModel();            
+	}
+   
+    private void InitializeComponent()
 		{
 			AvaloniaXamlLoader.Load(this);
 		}
 	}
-}
