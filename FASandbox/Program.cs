db9ebdb65c91f80dae201683681b92483b7b8426
amwx <40413319+amwx@users.noreply.github.com>
2022-08-25 17:26:00 -0400

    Prep sandbox app for testing

--- FASandbox/Program.cs
+++ FASandbox/Program.cs
@@ -12,9 +12,9 @@ internal class Program
 	public static void Main(string[] args) => BuildAvaloniaApp()
 		.StartWithClassicDesktopLifetime(args);
 
-	// Avalonia configuration, don't remove; also used by visual designer.
-	public static AppBuilder BuildAvaloniaApp()
-		=> AppBuilder.Configure<App>()
-			.UsePlatformDetect()
-			.LogToTrace(Avalonia.Logging.LogEventLevel.Debug, "TabView");
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .LogToTrace(Avalonia.Logging.LogEventLevel.Debug, "TabView");
 }
