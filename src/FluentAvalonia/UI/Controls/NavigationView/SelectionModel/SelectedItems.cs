5004361d7e5b80b1334ca01b9a0a0c4461a495c7
amwx <40413319+amwx@users.noreply.github.com>
2023-03-14 17:24:13 -0400

    Rewrite the SelectionModel for the NavigationView to follow WinUI more closely

--- src/FluentAvalonia/UI/Controls/NavigationView/SelectionModel/SelectedItems.cs
+++ src/FluentAvalonia/UI/Controls/NavigationView/SelectionModel/SelectedItems.cs
@@ -1,41 +1,100 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 
 namespace FluentAvalonia.UI.Controls;
 
-internal interface ISelectedItemInfo
+internal struct SelectedItemInfo
 {
-    public IndexPath Path { get; }
+    public SelectedItemInfo(SelectionNode node, IndexPath path)
+    {
+        Node = new WeakReference<SelectionNode>(node);
+        Path = path;
+    }
+
+    public WeakReference<SelectionNode> Node;
+    public IndexPath Path;
 }
 
-internal class SelectedItems<TValue, Tinfo> : IReadOnlyList<TValue>
-    where Tinfo : ISelectedItemInfo
+internal class SelectedItems<T> : IReadOnlyList<T>
 {
-    private readonly List<Tinfo> _infos;
-    private readonly Func<List<Tinfo>, int, TValue> _getAtImpl;
-
-    public SelectedItems(
-        List<Tinfo> infos,
-        int count,
-        Func<List<Tinfo>, int, TValue> getAtImpl)
+    public SelectedItems(IList<SelectedItemInfo> infos,
+        Func<IList<SelectedItemInfo>, int, T> getAtImpl)
     {
         _infos = infos;
         _getAtImpl = getAtImpl;
-        Count = count;
-    }
-
-    public TValue this[int index] => _getAtImpl(_infos, index);
-
-    public int Count { get; }
-
-    public IEnumerator<TValue> GetEnumerator()
-    {
-        for (var i = 0; i < Count; ++i)
+        foreach(var info in infos)
         {
-            yield return this[i];
+            if (info.Node.TryGetTarget(out var selNode))
+            {
+                _totalCount += selNode.SelectedCount;
+            }
+            else
+            {
+                throw new InvalidOperationException("Selection changed after the SelectedIndices/Items property was read");
+            }
         }
     }
 
+    public int Count => _totalCount;
+
+    public int Size() => Count;
+
+    public T this[int index] => GetAt(index);
+
+    public T GetAt(int index) => _getAtImpl(_infos, index);
+
+    public IEnumerator<T> GetEnumerator() => new Iterator<T>(this);
+
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    private IList<SelectedItemInfo> _infos;
+    private int _totalCount;
+    private Func<IList<SelectedItemInfo>, int, T> _getAtImpl;
+
+    private class Iterator<TInner> : IEnumerator<TInner>
+    {
+        public Iterator(IReadOnlyList<TInner> owner)
+        {
+            _owner = owner;
+        }
+
+        public TInner Current
+        {
+            get
+            {
+                var items = _owner;
+                if (_currentIndex < items.Count)
+                {
+                    return items.ElementAt(_currentIndex);
+                }
+
+                return default;
+            }
+        }
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+
+        }
+
+        public bool MoveNext()
+        {
+            if (_currentIndex < _owner.Count)
+            {
+                ++_currentIndex;
+                return (_currentIndex < _owner.Count);
+            }
+
+            return false;
+        }
+
+        public void Reset()
+        {
+
+        }
+
+        private IReadOnlyList<TInner> _owner;
+        private int _currentIndex;
+    }
 }
--- src/FluentAvalonia/UI/Controls/NavigationView/SelectionModel/SelectionModelSelectionChangedEventArgs.cs
+++ src/FluentAvalonia/UI/Controls/NavigationView/SelectionModel/SelectionModelSelectionChangedEventArgs.cs
@@ -1,39 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace FluentAvalonia.UI.Controls;
+﻿namespace FluentAvalonia.UI.Controls;
 
 internal class SelectionModelSelectionChangedEventArgs : EventArgs
 {
-    public SelectionModelSelectionChangedEventArgs(
-        IReadOnlyList<IndexPath> deselectedIndices,
-        IReadOnlyList<IndexPath> selectedIndices,
-        IReadOnlyList<object> deselectedItems,
-        IReadOnlyList<object> selectedItems)
-    {
-        DeselectedIndices = deselectedIndices ?? Array.Empty<IndexPath>();
-        SelectedIndices = selectedIndices ?? Array.Empty<IndexPath>();
-        DeselectedItems = deselectedItems ?? Array.Empty<object>();
-        SelectedItems = selectedItems ?? Array.Empty<object>();
-    }
 
-    /// <summary>
-    /// Gets the indices of the items that were removed from the selection.
-    /// </summary>
-    public IReadOnlyList<IndexPath> DeselectedIndices { get; }
-
-    /// <summary>
-    /// Gets the indices of the items that were added to the selection.
-    /// </summary>
-    public IReadOnlyList<IndexPath> SelectedIndices { get; }
-
-    /// <summary>
-    /// Gets the items that were removed from the selection.
-    /// </summary>
-    public IReadOnlyList<object> DeselectedItems { get; }
-
-    /// <summary>
-    /// Gets the items that were added to the selection.
-    /// </summary>
-    public IReadOnlyList<object> SelectedItems { get; }
 }
