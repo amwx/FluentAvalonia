ed20880670e518d7c7dabfc423ee923f3c0153ea
amwx <40413319+amwx@users.noreply.github.com>
2023-03-18 18:03:14 -0400

    Main page header changes

--- samples/FAControlsGallery/Controls/PageHeaderControl.cs
+++ samples/FAControlsGallery/Controls/PageHeaderControl.cs
@@ -3,14 +3,32 @@ using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Media.Imaging;
 using Avalonia.Platform;
+using Avalonia.Styling;
 
 namespace FAControlsGallery.Controls;
+
+public enum PageHeaderTextType
+{
+    Main,
+    CoreControls,
+    FAControls,
+}
 
 public class PageHeaderControl : TemplatedControl
 {
     public PageHeaderControl()
     {
         SizeChanged += OnSizeChanged;
+    }
+
+    public static readonly DirectProperty<PageHeaderControl, PageHeaderTextType> TextTypeProperty =
+        AvaloniaProperty.RegisterDirect<PageHeaderControl, PageHeaderTextType>(nameof(TextType),
+            x => x.TextType, (x, v) => x.TextType = v);
+
+    public PageHeaderTextType TextType
+    {
+        get => _textType;
+        set => SetAndRaise(TextTypeProperty, ref _textType, value);
     }
 
     public static readonly DirectProperty<PageHeaderControl, Uri> TitleTextImageProperty =
@@ -28,20 +46,16 @@ public class PageHeaderControl : TemplatedControl
         base.OnApplyTemplate(e);
 
         _text1 = e.NameScope.Get<Image>("TitleTextImageHost");
-        _text2 = e.NameScope.Get<Image>("VersionTextHost");
-        if (TitleTextImage != null)
-        {           
-            if (TitleTextImage.IsAbsoluteUri && TitleTextImage.IsFile)
-            {
-                _text1.Source = new Bitmap(TitleTextImage.LocalPath);
-            }
-            else
-            {
-                var al = AvaloniaLocator.Current.GetService<IAssetLoader>();
-                using var s = al.Open(TitleTextImage);
-                _text1.Source = new Bitmap(s);
-            }
-        }        
+        UpdateTitleText();
+    }
+
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+    {
+        base.OnPropertyChanged(change);
+        if (change.Property == ActualThemeVariantProperty)
+        {
+            UpdateTitleText();
+        }
     }
 
     private void OnSizeChanged(object sender, SizeChangedEventArgs e)
@@ -51,18 +65,45 @@ public class PageHeaderControl : TemplatedControl
         {
             var delta = 630 - wid;
 
-            _text1.Width = 300 - delta;
-            _text2.Width = 90 - (delta * 0.5);
+            _text1.Width = 400 - delta;
         }
         else
         {
-            _text1.Width = _text2.Width = double.NaN;
+            _text1.Width = double.NaN;
         }
 
         PseudoClasses.Set(":small", wid < 450);
     }
 
+    private void UpdateTitleText()
+    {
+        if (_text1 == null)
+            return;
+
+        var theme = ActualThemeVariant;
+
+        const string asset = "avares://FAControlsGallery/Assets/Images/";
+
+        var header = TextType switch
+        {
+            PageHeaderTextType.CoreControls => "FAHeader_CoreControls",
+            PageHeaderTextType.FAControls => "FAHeader_NewControls",
+            _ => "FAHeader2"
+        };
+
+        if (theme == ThemeVariant.Light)
+        {
+            header += "_Dark";
+        }
+
+        header += ".png";
+
+        var al = AvaloniaLocator.Current.GetService<IAssetLoader>();
+        using var s = al.Open(new Uri($"{asset}{header}"));
+        _text1.Source = new Bitmap(s);
+    }
+
     private Uri _titleTextImage;
     private Image _text1;
-    private Image _text2;
+    private PageHeaderTextType _textType;
 }
