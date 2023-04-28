8184a5b92997721f9cf0cbad6a438805fe09e4f1
amwx <40413319+amwx@users.noreply.github.com>
2023-04-28 17:31:05 -0400

    Always align TaskDialogCommands

--- src/FluentAvalonia/UI/Controls/TaskDialog/TaskDialog.cs
+++ src/FluentAvalonia/UI/Controls/TaskDialog/TaskDialog.cs
@@ -507,6 +507,8 @@ public partial class TaskDialog : ContentControl
         List<Control> commands = new List<Control>();
 
         bool foundDefault = _defaultButton != null;
+        int iconCount = 0;
+        int normalCommandCount = 0;
         for (int i = 0; i < _commands.Count; i++)
         {
             if (_commands[i] is TaskDialogCheckBox tdcb)
@@ -521,7 +523,7 @@ public partial class TaskDialog : ContentControl
 
                 com.Classes.Add(s_cFATDCom);
 
-                commands.Add(com);
+                commands.Add(com);                
             }
             else if (_commands[i] is TaskDialogRadioButton tdrb)
             {
@@ -560,6 +562,21 @@ public partial class TaskDialog : ContentControl
                 }
 
                 commands.Add(com);
+
+                // Icons are only supported on "normal" TaskDialogCommands
+                if (tdc.IconSource != null)
+                    iconCount++;
+                normalCommandCount++;
+            }
+        }
+
+        if (iconCount != normalCommandCount)
+        {
+            // We have an item with no icon - force it to display as if one
+            // was present so that its aligned with the others
+            for (int i = 0; i < commands.Count; i++)
+            {
+                (commands[i].Classes as IPseudoClasses).Set(SharedPseudoclasses.s_pcIcon, true);
             }
         }
 
