6ac4f0121b06b2e8f5e8df36234ec1a77c7ea2cf
amwx <40413319+amwx@users.noreply.github.com>
2023-05-01 15:51:05 -0400

    Highly, Highly experimental ConnectedAnimationService

---
+++ src/FluentAvalonia/UI/Controls/Experimental/ConnectedAnimationConfiguration.cs
@@ -0,0 +1,10 @@
+﻿// NOTE: This class is experimental and will not receive support.
+// Use at your own risk
+
+namespace FluentAvalonia.UI.Controls.Experimental;
+
+public abstract class ConnectedAnimationConfiguration { }
+
+public class DirectConnectedAnimationConfiguration : ConnectedAnimationConfiguration { }
+
+public class GravityConnectedAnimationConfiguration : ConnectedAnimationConfiguration { }
