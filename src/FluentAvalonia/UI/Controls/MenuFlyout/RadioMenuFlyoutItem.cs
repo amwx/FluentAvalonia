1c6bd526ebea9829460182df42b11b38b0d99f77
amwx <40413319+amwx@users.noreply.github.com>
2023-05-31 22:48:00 -0400

    Add hack fix for RadioMenuFlyoutItem

--- src/FluentAvalonia/UI/Controls/MenuFlyout/RadioMenuFlyoutItem.cs
+++ src/FluentAvalonia/UI/Controls/MenuFlyout/RadioMenuFlyoutItem.cs
@@ -96,6 +96,9 @@ public class RadioMenuFlyoutItem : MenuFlyoutItem
             {
                 if (group.TryGetTarget(out var item))
                 {
+                    if (item == this)
+                        return;
+
                     item.UncheckFromGroupSelection();
                 }
 
