9c345b41a36526f9600706f0980c01e5b0930e08
Leon V <leon99@outlook.com>
2023-06-25 11:52:09 +1000

    Improve readability for demo notes

--- samples/FAControlsGallery/ViewModels/CoreControlsPageViewModel.cs
+++ samples/FAControlsGallery/ViewModels/CoreControlsPageViewModel.cs
@@ -7,7 +7,7 @@ public class CoreControlsPageViewModel : MainPageViewModelBase
 {
     public CoreControlsPageViewModel(string coreControlsJson)
     {
-        NoteText = "Fluent v2 styling on Avalonia controls. Only controls that have styling changes" +
+        NoteText = "Fluent v2 styling on Avalonia controls. Only controls that have styling changes " +
             "are shown here for demo purposes, though all controls (except ContextMenu) are supported.";
 
         CoreControlGroups = JsonSerializer.Deserialize<List<PageBaseViewModel>>(coreControlsJson);
