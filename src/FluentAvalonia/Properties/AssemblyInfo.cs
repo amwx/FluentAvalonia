4c22d94e67ff658fa19f5865ee3214ac123c2806
胡锦广 <hjg1023@outlook.com>
2023-08-05 03:13:11 +0800

    Add XML Namespace Definitions

--- src/FluentAvalonia/Properties/AssemblyInfo.cs
+++ src/FluentAvalonia/Properties/AssemblyInfo.cs
@@ -1,3 +1,11 @@
 ﻿using System.Runtime.CompilerServices;
 
+using Avalonia.Metadata;
+
 [assembly: InternalsVisibleTo("FluentAvaloniaTests")]
+
+[assembly: XmlnsDefinition("https://github.com/fluent-avalonia", "FluentAvalonia.UI")]
+[assembly: XmlnsDefinition("https://github.com/fluent-avalonia", "FluentAvalonia.UI.Controls")]
+[assembly: XmlnsDefinition("https://github.com/fluent-avalonia", "FluentAvalonia.UI.Controls.Primitives")]
+[assembly: XmlnsDefinition("https://github.com/fluent-avalonia", "FluentAvalonia.UI.Windowing")]
+[assembly: XmlnsDefinition("https://github.com/fluent-avalonia", "FluentAvalonia.Styling")]
