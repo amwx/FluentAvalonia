9de1b214db835fd48ff2daacb0fd73d290bf4b6a
amwx <40413319+amwx@users.noreply.github.com>
2023-09-07 17:03:38 -0400

    Sample page

---
+++ samples/FAControlsGallery/Pages/FAControlsPages/RangeSliderPage.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia.Controls;
+using FluentAvalonia.UI.Controls;
+
+namespace FAControlsGallery.Pages;
+
+public partial class RangeSliderPage : ControlsPageBase
+{
+    public RangeSliderPage()
+    {
+        InitializeComponent();
+
+        TargetType = typeof(RangeSlider);
+    }
+}
