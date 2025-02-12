10dac09a070715edc73352ab5100728001af756b
amwx <40413319+amwx@users.noreply.github.com>
2025-02-12 13:47:19 -0500

    Use width with trailing whitespace for FontIcon/SymbolIcon

--- src/FluentAvalonia/UI/Controls/IconElement/FontIcon.cs
+++ src/FluentAvalonia/UI/Controls/IconElement/FontIcon.cs
@@ -48,8 +48,8 @@ public partial class FontIcon : FAIconElement
         var dstRect = new Rect(Bounds.Size);
         using (context.PushClip(dstRect))
         {
-            var pt = new Point(dstRect.Center.X - _textLayout.Width / 2,
-                               dstRect.Center.Y - _textLayout.Height / 2);
+            var pt = new Point(dstRect.Center.X - _textLayout.WidthIncludingTrailingWhitespace * 0.5,
+                               dstRect.Center.Y - _textLayout.Height * 0.5);
             _textLayout.Draw(context, pt);
         }
     }
