0bb1fb69d7b574835526e78d4b89f062d61ffbf9
DAVENEL Thomas <thomas.davenel@scalian.com>
2025-05-05 09:55:32 +0200

    amwx requested changes

--- src/FluentAvalonia/UI/Controls/NumberBox/NumberBox.cs
+++ src/FluentAvalonia/UI/Controls/NumberBox/NumberBox.cs
@@ -242,15 +242,7 @@ public partial class NumberBox : TemplatedControl
         if (_textBox == null)
             return;
 
-        // Validate the text of the inner textbox
-        if (_textBox.Text == null)
-        {
-            // Override text to current value
-            UpdateTextToValue();
-            return;
-        }
-
-        var text = _textBox.Text.Trim();
+        var text = _textBox.Text?.Trim();
 
         // Handles empty TextBox case, set text ot current value
         if (string.IsNullOrEmpty(text))
