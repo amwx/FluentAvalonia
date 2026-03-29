b704952dab35e439ba0727998fe9d8fc6188660b
amwx <40413319+amwx@users.noreply.github.com>
2026-03-29 14:03:59 -0400

    Unit Test helpers updated (oops forgot in prev commit)

--- tests/FluentAvaloniaTests/Helpers/MouseHelpers.cs
+++ tests/FluentAvaloniaTests/Helpers/MouseHelpers.cs
@@ -18,16 +18,6 @@ public static class MouseHelpers
         window.MouseUp(point, button, modifiers);
     }
 
-    public static void MouseDownControl(this Control ctrl, Point localPoint, MouseButton button, RawInputModifiers modifiers = RawInputModifiers.None)
-    {
-        var window = TopLevel.GetTopLevel(ctrl);
-
-        var matrix = ctrl.TransformToVisual(window).Value;
-        var point = localPoint.Transform(matrix);
-
-        window.MouseDown(point, button, modifiers);
-    }
-
     public static void MoveMouseToControl(this Control ctrl, Point localPoint)
     {
         var window = TopLevel.GetTopLevel(ctrl);
@@ -36,15 +26,5 @@ public static class MouseHelpers
         var point = localPoint.Transform(matrix);
 
         window.MouseMove(point);
-    }
-
-    public static void MouseUpControl(this Control ctrl, Point localPoint, MouseButton button, RawInputModifiers modifiers = RawInputModifiers.None)
-    {
-        var window = TopLevel.GetTopLevel(ctrl);
-
-        var matrix = ctrl.TransformToVisual(window).Value;
-        var point = localPoint.Transform(matrix);
-
-        window.MouseUp(point, button, modifiers);
     }
 }
