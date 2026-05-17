de1d85defffa45edc2ed48687fe32f2b90a93b61
amwx <40413319+amwx@users.noreply.github.com>
2026-05-16 21:31:04 -0400

    Start adding some ItemsRepeater tests

---
+++ tests/FluentAvaloniaTests/ControlTests/RepeaterTests/RepeaterTestContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Avalonia.Controls;
+using Avalonia.Threading;
+using FluentAvalonia.UI.Controls;
+
+namespace FluentAvaloniaTests.ControlTests;
+
+internal class RepeaterTestContext : IDisposable
+{
+    public RepeaterTestContext()
+    {
+        ItemsRepeater = new FAItemsRepeater();
+        ScrollViewer = new ScrollViewer
+        {
+            Content = ItemsRepeater
+        };
+        Window = new Window
+        {
+            Content = ScrollViewer
+        };
+
+        Window.Show();
+        Window.UpdateLayout();
+        Dispatcher.UIThread.RunJobs();
+    }
+
+    public Window Window { get; }
+
+    public ScrollViewer ScrollViewer { get; }
+
+    public FAItemsRepeater ItemsRepeater { get; }
+
+    public void Dispose()
+    {
+        Window.Close();
+    }
+}
